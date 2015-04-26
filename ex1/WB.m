030942a2577378ee66a1826a9a8747d80807659e
gil <gilovi@users.noreply.github.com>
2015-04-26 21:28:20 +0300

    bustop 1

--- ex1/WB.m
+++ ex1/WB.m
@@ -1,4 +1,4 @@
-function [ output_args ] = WB( orig, flashed ,gray_card, method_num )
+function [ WB_pic ] = WB( orig, flashed ,gray_card, method_num )
 %WB generates White balanced picture from provided original,
 %   flashed and gray card photos. 
 %   the function will open a dialog for the user to mark the
@@ -6,12 +6,14 @@ function [ output_args ] = WB( orig, flashed ,gray_card, method_num )
 %   works automatically since.
 %
 %   inputs: 
-%           orig: the original picture.
-%           flashed: the picture with flash.
+%           orig: the original picture in xyz format.
+%           flashed: the picture with flash in xyz format.
 %           geay_card: a picture with a grah card and only the flash as
-%           light source.
-%           method_num: the method to preform the WB 1- bradford, 2-
-%           von-kries.
+%           light source in xyz format.
+%           method_num: the method to preform the WB-
+%           1- bradford.
+%           2- von-kries.
+%           3- xyz scale
 %           
 %   output:
 %           the white balanced piucture.
@@ -19,7 +21,14 @@ function [ output_args ] = WB( orig, flashed ,gray_card, method_num )
 %   
 %
 
+brad = [.8951, .2664,-.1614;-.7502,1.7135,0.0367;0.0389,-0.0685,1.0296];
+von_kries = [0.40024,0.7076,-0.0808100;-0.2263,1.16532,0.0457;0,0,0.91822];
 
+if (method_num)
+
+h = vision.GammaCorrector(2.2,'Correction','Gamma');
+cNo=step(h,mat2gray(no));
+y = step(h,x);
 
 end
 
--- ex1/get_light_source.m
+++ ex1/get_light_source.m
@@ -11,14 +11,18 @@ function [ L1 ] = get_light_source( I1, I2 ,L2 )
 %   for ferther information refer to the document.
 
 L2_m = permute(L2,[3,2,1]);
-L2_m = repmat(L2_m,size(I1));
+[r,c,~]= size(I1);
+L2_m = repmat(L2_m,[r,c]);
 
 L1k = I1.*L2_m./(I1-I2);
 
-G = L1k(:,:,2)./L1k(:,:,1);
+G = (L1k(:,:,2)./L1k(:,:,1));
 B = L1k(:,:,3)./L1k(:,:,1);
 
-L1 = [1,G,B]';
+G(isnan(G)) = 0;
+
+
+L1 = [1,mean(G(~isnan(G)&~isinf(G))),mean(B(~isnan(B)&~isinf(B)))]';
 
 end
 
