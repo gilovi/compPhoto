f71f415f5d7c9768b768db15c5663b5cc55a58fc
gilovi <gilovi@users.noreply.github.com>
2015-07-22 12:02:11 +0300

    more tests needed

--- ex3/move.m
+++ ex3/move.m
@@ -1,66 +1,59 @@
-function [ moved ] = move( images , flowVec , theta, beta )
+function [ moved ] = move( images , flowVec , degrees, beta )
 %MOVE
-%   theta from 0 to -pi
-%beta from -1 to 1
+%   degrees 0 to 180
+%   beta from -1 to 1
 
-%picAmount = floor(floor(length(images)/2)/amount);
-[r,c] = size(images{1});
+    [r,c] = size(images{1});
+    isNeg = any(flowVec(flowVec < 0));
+    center = floor(((beta+1)/2) * (length(images)-1)) + 1; % normalizing beta to be in #images scale
+    theta = degrees/180*-pi;
+    
+    cumflow = [0; abs(cumsum(flowVec))];
+    translation = cumflow - cumflow(center);
+    pixVal = tan(theta)*translation;
+    pixVal = pixVal/max(cumflow) * -r + 1; %normalizing and setting the sign.
+    pixVal = pixVal + (cumflow(center)/max(cumflow)) * r;
+    if isNeg
+        pixVal = pixVal * -1;
+    end
 
-q3 = theta<-pi/2;
-isNeg = any(flowVec(flowVec < 0));
+    % posDist = conv(pixVal,[-1,1]);
+    % posDist(1) = 0;
+    % posDist(length(posDist)) = 0;
+    % posDist = posDist/2;
+    % 
+    % shiftDist = conv(cumflow,[-1,1]); 
+    % shiftDist(1) = 0;
+    % shiftDist(length(shiftDist)) = 0;
+    % shiftDist = shiftDist/2;
 
-cumflow = [0; abs(cumsum(flowVec))];
-center = floor(((beta+1)/2) * (length(images)-1)) + 1; % normalizing beta to be in #images scale
+    dist = conv(pixVal,[-1,1]) + conv(cumflow,[-1,1]);
+    dist(1) = 0;
+    dist(length(dist)) = 0;
+    dist = dist/2;
 
-translation = cumflow - cumflow(center);
-pixVal = tan(theta)*translation;
-%[~,i] = max(abs(y));
-pixVal = pixVal/max(cumflow) * -r + 1; %normalizing and setting the sign.
-pixVal = pixVal + (cumflow(center)/max(cumflow)) * r;
-if isNeg
-    pixVal = pixVal * -1;
-end
+    %%%%%%%%
+    figure;plot(pixVal, 1:length(translation) ,'-');
+    grid on; grid minor; hold on; axis([1,r,1,length(translation)]);
+    plot(pixVal , 1:length(translation),'or' );
+    %%%%%%%%
+    
+    % TODO: restrict the pixval and translation to its limits 
+    
+    moved = zeros(sum(ceil(dist*2)), c);
+    for i = 1 : length(images)
+         cur = images{i};
+    %   Ldist = dist(i);
+    %   Rdist = dist(i+1);
+         iL = ceil(dist(i));
+         iR = floor(dist(i+1)) ;
+         rL = dist(i) - floor(dist(i));
+         rR = dist(i+1) - floor(dist(i+1));
 
-posDist = conv(pixVal,[-1,1]);
-posDist = posDist(2:length(posDist)-1)/2;
-shiftDist = conv(cumflow,[-1,1]); 
-shiftDist = conv(shiftDist,[-1,1])/2;
+         moved(pixVal(i)-iL : pixVal(i)+iR ) = cur(pixVal(i)-iL : pixVal(i)+iR);
+    %merge reminders:
+         moved(pixVal(i) - ceil(iL+rL)) = cur(pixVal(i) - ceil(iL+rL))*(1-rL);
+         moved(pixVal(i) + ceil(iR+rR)) = cur(pixVal(i) +  ceil(iR+rR))*(rR+~rR);
+    end
 
-%%%%%%%%
-figure;plot(pixVal, 1:length(translation) ,'-');
-grid on; grid minor; hold on; axis([1,r,1,length(translation)]);
-plot(pixVal , 1:length(translation),'or' );
-%%%%%%%%
-
-
-% y = tan(theta)*cumflow + beta;
-% %[~,centerIm] = min (abs((((c-1)/2)+(direction*((c-1)/2))+1) - cumPixflow));
-% %dist = min(center,length(images) - center );
-% 
-% fl = floor(cumPixflow);
-% cl = ceil(cumPixflow);
-% rem = cumPixflow - fl;
-
-
-
-% TODO: restrict the pixval and translation to its limits 
-moved = zeros(sum(posDist*2) + sum(shiftDist*2), c);
-for i = 1 : length(images)
-     cur = images{i};
-%     %int part
-%     ipart = cl(i):fl(i+1);
-%     moved(ipart) = cur(ipart);
-%     %merge reminders:
-%     if i < length(images)
-%         moved(:,cl(i+1),:) = cur(:,cl(i+1),:)*(rem(i+1));
-end
-%     
-%     if rem(i) ~= 0
-%       moved(:,fl(i),:) = moved(:,fl(i),:) + cur(:,fl(i),:)*(1-rem(i));
-%     end
-%     
-% end
-% 
-% 
-% end
-% 
+end 
