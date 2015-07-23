cf5069545bcc31ef7d7e7183da9d64bb9ef233de
gilovi <gilovi@users.noreply.github.com>
2015-07-23 18:47:48 +0300

    final.

--- ex3/better_gui.m
+++ ex3/better_gui.m
@@ -22,7 +22,7 @@ function varargout = better_gui(varargin)
 
 % Edit the above text to modify the response to help better_gui
 
-% Last Modified by GUIDE v2.5 23-Jul-2015 11:33:54
+% Last Modified by GUIDE v2.5 23-Jul-2015 14:36:44
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -85,6 +85,7 @@ function load_Callback(hObject, eventdata, handles)
 I = get_images(handles.fileName, handles.pathName);
 handles.I = I;
 handles.optical_flow=optical_flow(I);
+imshow(I{1});
 guidata(hObject,handles);
 
 
@@ -261,6 +262,7 @@ function beta_Callback(hObject, eventdata, handles)
 %        str2double(get(hObject,'String')) returns contents of beta as a double
 
 handles.beta= str2double(get(hObject,'String'));
+handles.beta_int=handles.beta;
 q=move( handles.I, handles.optical_flow, handles.degrees, handles.beta );
 imshow(q);
 guidata(hObject,handles);
@@ -342,11 +344,14 @@ function r90_Callback(hObject, eventdata, handles)
 % Hint: get(hObject,'Value') returns toggle state of r90
 handles.r90 = 1;
 handles.num=90;
+global beta;
 %set(handles.r135,'Value', 0);
 %set(handles.r45,'Value', 0);
+global num;
+num=90;
 set(handles.degrees,'String', '90');
 %set(handles.deg_slide,'Value', 90);
-q=move( handles.I, handles.optical_flow, 90, handles.beta );
+q=move( handles.I, handles.optical_flow, 90, beta );
 imshow(q);
 guidata(hObject,handles);
 
@@ -357,14 +362,16 @@ function r45_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of r45
-
+global num;
+global beta;
+num=45;
 handles.r45 = 1;
 handles.num=45;
 %set(handles.r135,'Value', 0);
 %set(handles.r90,'Value', 0);
 set(handles.degrees,'String', '45');
 %set(handles.deg_slide,'Value', 45);
-q=move( handles.I, handles.optical_flow, 45, handles.beta );
+q=move( handles.I, handles.optical_flow, 45, beta );
 imshow(q);
 guidata(hObject,handles);
 
@@ -377,7 +384,9 @@ function r135_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of r135
-
+global num;
+global beta;
+num=135;
 %handles.r45 = 0;
 %handles.r90 = 0;
 handles.r135 = 1;
@@ -386,7 +395,7 @@ handles.num=135;
 %set(handles.r45,'Value', 0);
 set(handles.degrees,'String', '135');
 %set(handles.deg_slide,'Value', 135);
-q=move( handles.I, handles.optical_flow, 135, handles.beta );
+q=move( handles.I, handles.optical_flow, 135, beta );
 imshow(q);
 
 guidata(hObject,handles);
@@ -400,10 +409,14 @@ function deg_slide_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
+global num;
+global beta;
 deg=get(hObject,'Value');
+num=deg;
 handles.deg_slide=deg;
-set(handles.degrees,'String', num2str(deg));
+%set(num,'String', num2str(deg));
+q=move( handles.I, handles.optical_flow, num, beta );
+imshow(q);
 guidata(hObject,handles);
 
 
@@ -428,8 +441,11 @@ function pos_box_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of pos_box as text
 %        str2double(get(hObject,'String')) returns contents of pos_box as a double
+global num;
+global beta;
+beta= handles.pos_box;
 handles.pos_box=str2double(get(hObject,'String'));
-q=move( handles.I, handles.optical_flow, handles.degrees, handles.beta );
+q=move( handles.I, handles.optical_flow, num,beta );
 imshow(q);
 guidata(hObject,handles);
 
@@ -447,3 +463,35 @@ function pos_box_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+
+% --- Executes on slider movement.
+function beta_slide_Callback(hObject, eventdata, handles)
+% hObject    handle to beta_slide (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+global num;
+global beta;
+handles.beta_slide=get(hObject,'Value');
+beta=handles.beta_slide;
+set(handles.beta,'String', num2str(handles.beta_slide));
+handles.beta_int=handles.beta_slide;
+q=move( handles.I, handles.optical_flow, num, handles.beta_int );
+imshow(q);
+guidata(hObject,handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function beta_slide_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to beta_slide (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
--- ex3/focus.m
+++ ex3/focus.m
@@ -12,24 +12,22 @@ for i=1:s
     imArry(:,:,:,i) = shiftIm(I{i},steps(i)*shiftVal);   
 end
     if remOc
-        Im=zeros(h,w,d);
-        for i=1:s
-            a = imArry(:,:,:,i);
-            M(:,i) = arrayfun(@(idx) norm(permute(a(:,idx,:),[3 1 2])), 1:size(a,2));
-        end
-        mn = mean(M,2);
-        for i=1:w
-           mat = M(i,:);
-           [~ ,p] = min(abs(mat-mn(i)));
-           tmp = imArry(:,i,:,p);
-           Im(:,i,:) = tmp;
-        end
-        %Im = median(imArry,4);
+%         Im=zeros(h,w,d);
+%         for i=1:s
+%             a = imArry(:,:,:,i);
+%             M(:,i) = arrayfun(@(idx) norm(permute(a(:,idx,:),[3 1 2])), 1:size(a,2));
+%         end
+%         mn = mean(M,2);
+%         for i=1:w
+%            mat = M(i,:);
+%            [~ ,p] = min(abs(mat-mn(i)));
+%            tmp = imArry(:,i,:,p);
+%            Im(:,i,:) = tmp;
+%         end
+        Im = median(imArry,4);
     else
         Im = mean(imArry,4);
     end
     
     
 end
-
-
