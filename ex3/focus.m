074b20ac3912aac17be2f858c562e77e66ac944c
gilovi <gilovi@users.noreply.github.com>
2015-07-23 16:55:21 +0300

    better focus?

--- ex3/focus.m
+++ ex3/focus.m
@@ -11,21 +11,22 @@ M = zeros(w,s);
 for i=1:s
     imArry(:,:,:,i) = shiftIm(I{i},steps(i)*shiftVal);   
 end
-    if remOc 
-        Im = median(imArry,4);
-    else
-        Im = mean(imArry,4);
-
+    if remOc
+        Im=zeros(h,w,d);
         for i=1:s
             a = imArry(:,:,:,i);
             M(:,i) = arrayfun(@(idx) norm(permute(a(:,idx,:),[3 1 2])), 1:size(a,2));
         end
-        mn = mean(M,1);
+        mn = mean(M,2);
         for i=1:w
            mat = M(i,:);
-           p=find(mat==mn(i),1);
-           Im(:,i,:)=imArry(:,i,:,p);
+           [~ ,p] = min(abs(mat-mn(i)));
+           tmp = imArry(:,i,:,p);
+           Im(:,i,:) = tmp;
         end
+        %Im = median(imArry,4);
+    else
+        Im = mean(imArry,4);
     end
     
     
